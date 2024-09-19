7b97c8a4cab43cf19c4473a83f30b5ef04840586
Louise Söderström <louise.matematik@gmail.com>
2024-09-19 14:34:30 +0200

    Implement GQLSTATUS 22003 (cause 51N52)

--- community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/AdministrationCommand.scala
+++ community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/AdministrationCommand.scala
@@ -68,6 +68,10 @@ import org.neo4j.cypher.internal.util.symbols.CTList
 import org.neo4j.cypher.internal.util.symbols.CTMap
 import org.neo4j.cypher.internal.util.symbols.CTNode
 import org.neo4j.cypher.internal.util.symbols.CTString
+import org.neo4j.gqlstatus.ErrorClassification
+import org.neo4j.gqlstatus.ErrorGqlStatusObjectImplementation
+import org.neo4j.gqlstatus.GqlParams
+import org.neo4j.gqlstatus.GqlStatusInfoCodes
 
 sealed trait AdministrationCommand extends StatementWithGraph with SemanticAnalysisTooling {
 
@@ -188,7 +192,18 @@ sealed trait WriteAdministrationCommand extends AdministrationCommand {
 
     def numPrimaryGreaterThanZero(topology: Topology): SemanticCheck =
       if (topology.primaries.exists(_ < 1)) {
+        val gql = ErrorGqlStatusObjectImplementation.from(GqlStatusInfoCodes.STATUS_22003)
+          .withClassification(ErrorClassification.CLIENT_ERROR)
+          .atPosition(position.line, position.column, position.offset)
+          .withCause(ErrorGqlStatusObjectImplementation.from(GqlStatusInfoCodes.STATUS_51N52)
+            .withClassification(ErrorClassification.CLIENT_ERROR)
+            .atPosition(position.line, position.column, position.offset)
+            .withParam(GqlParams.NumberParam.count, topology.primaries.get)
+            .withParam(GqlParams.NumberParam.upper, 11)
+            .build())
+          .build()
         error(
+          gql,
           s"Failed to $command with `${Prettifier.extractTopology(topology).trim}`, PRIMARY must be greater than 0.",
           position
         )
