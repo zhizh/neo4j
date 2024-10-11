19c95db5d82eb18421e2824f90ba4d54cbe5e322
Rob Steward <speaktorob@gmail.com>
2024-10-11 16:55:18 +0100

    Fix ExpressionStringifier

--- community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/prettifier/ExpressionStringifier.scala
+++ community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/prettifier/ExpressionStringifier.scala
@@ -59,6 +59,7 @@ import org.neo4j.cypher.internal.expressions.GreaterThan
 import org.neo4j.cypher.internal.expressions.GreaterThanOrEqual
 import org.neo4j.cypher.internal.expressions.HasALabel
 import org.neo4j.cypher.internal.expressions.HasALabelOrType
+import org.neo4j.cypher.internal.expressions.HasAnyDynamicLabel
 import org.neo4j.cypher.internal.expressions.HasAnyLabel
 import org.neo4j.cypher.internal.expressions.HasDynamicLabels
 import org.neo4j.cypher.internal.expressions.HasLabels
@@ -336,6 +337,10 @@ private class DefaultExpressionStringifier(
 
       case HasAnyLabel(arg, labels) =>
         val l = labels.map(apply).mkString(":", "|", "")
+        s"${inner(ast)(arg)}$l"
+
+      case HasAnyDynamicLabel(arg, labels) =>
+        val l = labels.map(apply).map(l => s":$$any($l)").mkString
         s"${inner(ast)(arg)}$l"
 
       case HasALabel(arg) =>
